4b0ade1179afbc8be8efdb98b88df3c3715c9364
Garik Ambartsumyan <rq1991@mail.ru>
2016-04-12 23:34:03 +0400

    First test commit: query in AsyncTasks, needs refactoring

---
+++ dyploma/app/src/main/java/com/dyploma/garik/dyploma/LoadTask.java
@@ -0,0 +1,38 @@
+package com.dyploma.garik.dyploma;
+
+import android.os.AsyncTask;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by Harry on 12.04.2016.
+ */
+public class LoadTask extends AsyncTask<String, Void, String> {
+
+    protected String doInBackground(String... urls) {
+        try {
+            URL url = new URL(urls[0]);
+            HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.connect();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream(), "windows-1251"));
+            StringBuilder response = new StringBuilder();
+            String inputLine;
+            while ((inputLine = reader.readLine()) != null) {
+                response.append(inputLine);
+            }
+            System.out.println("yyy result = " + response);
+            return response.toString();
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    protected void onPostExecute(String feed) {
+        // TODO: check this.exception
+        // TODO: do something with the feed
+    }
+}
--- dyploma/app/src/main/java/com/dyploma/garik/dyploma/MainActivity.java
+++ dyploma/app/src/main/java/com/dyploma/garik/dyploma/MainActivity.java
@@ -2,12 +2,36 @@ package com.dyploma.garik.dyploma;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        try {
+            LoadTask myTask = new LoadTask();
+            myTask.execute(getResources().getString(R.string.load_query));
+            Toast.makeText(this,  "load_query = " + myTask.get(),
+                    Toast.LENGTH_LONG).show();
+
+            LoadTask myTask2 = new LoadTask();
+
+            myTask2.execute(getResources().getString(R.string.number_query));
+            Toast.makeText(this,  "number_query = " + myTask2.get(),
+                    Toast.LENGTH_LONG).show();
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+        ;
     }
 }
